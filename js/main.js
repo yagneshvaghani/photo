3b16faa006660adbd55374fbec2e6811428a8415
yagneshvaghani <71327442+yagneshvaghani@users.noreply.github.com>
2021-05-18 08:05:52 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function toggle(){
+    var sec =document.getElementById('section1');
+    var nav =document.getElementById('navbar');
+     sec.classList.toggle('active')
+     nav.classList.toggle('active')
+}
